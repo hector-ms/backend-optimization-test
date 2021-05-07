d65717f2e6a2535b939f45a7aedc914857d5f4cf
Héctor Muñoz <hector.9.1995@gmail.com>
2021-05-07 15:25:35 +0200

    Optimización de condicionales

--- app/src/Service/GildedRoseUpdateQualityService.php
+++ app/src/Service/GildedRoseUpdateQualityService.php
@@ -20,28 +20,20 @@ class GildedRoseUpdateQualityService
         $this->items = $items;
     }
 
-    function update_quality() :void
+    function update_quality(): void
     {
         foreach ($this->items as $item) {
             if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
+                if ($item->quality > 0 && $item->name != 'Sulfuras, Hand of Ragnaros') {
+                    $item->quality = $item->quality - 1;
                 }
             } else {
                 if ($item->quality < 50) {
                     $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
+                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert' && $item->sell_in < 11) {
+                        $item->quality = $item->quality + 1;
                         if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
+                            $item->quality = $item->quality + 1;
                         }
                     }
                 }
@@ -54,10 +46,8 @@ class GildedRoseUpdateQualityService
             if ($item->sell_in < 0) {
                 if ($item->name != 'Aged Brie') {
                     if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
+                        if ($item->quality > 0 && $item->name != 'Sulfuras, Hand of Ragnaros') {
+                            $item->quality = $item->quality - 1;
                         }
                     } else {
                         $item->quality = $item->quality - $item->quality;
