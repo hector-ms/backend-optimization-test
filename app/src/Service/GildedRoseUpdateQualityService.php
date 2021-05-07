58b84a061cea803b000e6406a7d6770cf87fca03
Héctor Muñoz <hector.9.1995@gmail.com>
2021-05-07 15:28:10 +0200

    Optimización de condicionales

--- app/src/Service/GildedRoseUpdateQualityService.php
+++ app/src/Service/GildedRoseUpdateQualityService.php
@@ -27,14 +27,12 @@ class GildedRoseUpdateQualityService
                 if ($item->quality > 0 && $item->name != 'Sulfuras, Hand of Ragnaros') {
                     $item->quality = $item->quality - 1;
                 }
-            } else {
-                if ($item->quality < 50) {
+            } else if ($item->quality < 50) {
+                $item->quality = $item->quality + 1;
+                if ($item->name == 'Backstage passes to a TAFKAL80ETC concert' && $item->sell_in < 11) {
                     $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert' && $item->sell_in < 11) {
+                    if ($item->sell_in < 6) {
                         $item->quality = $item->quality + 1;
-                        if ($item->sell_in < 6) {
-                            $item->quality = $item->quality + 1;
-                        }
                     }
                 }
             }
@@ -45,17 +43,13 @@ class GildedRoseUpdateQualityService
 
             if ($item->sell_in < 0) {
                 if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0 && $item->name != 'Sulfuras, Hand of Ragnaros') {
-                            $item->quality = $item->quality - 1;
-                        }
-                    } else {
+                    if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
                         $item->quality = $item->quality - $item->quality;
+                    } else if ($item->quality > 0 && $item->name != 'Sulfuras, Hand of Ragnaros') {
+                        $item->quality = $item->quality - 1;
                     }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
+                } else if ($item->quality < 50) {
+                    $item->quality = $item->quality + 1;
                 }
             }
         }
