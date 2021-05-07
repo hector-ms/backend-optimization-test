fd17c860f53d1be3629e2e08f398938ae3db8ca2
Héctor Muñoz <hector.9.1995@gmail.com>
2021-05-07 15:29:15 +0200

    Optimización de condicionales

--- app/src/Service/GildedRoseUpdateQualityService.php
+++ app/src/Service/GildedRoseUpdateQualityService.php
@@ -23,13 +23,13 @@ class GildedRoseUpdateQualityService
     function update_quality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0 && $item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
+                if ($item->quality > 0 && $item->name !== 'Sulfuras, Hand of Ragnaros') {
                     $item->quality = $item->quality - 1;
                 }
             } else if ($item->quality < 50) {
                 $item->quality = $item->quality + 1;
-                if ($item->name == 'Backstage passes to a TAFKAL80ETC concert' && $item->sell_in < 11) {
+                if ($item->name === 'Backstage passes to a TAFKAL80ETC concert' && $item->sell_in < 11) {
                     $item->quality = $item->quality + 1;
                     if ($item->sell_in < 6) {
                         $item->quality = $item->quality + 1;
@@ -37,15 +37,15 @@ class GildedRoseUpdateQualityService
                 }
             }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
                 $item->sell_in = $item->sell_in - 1;
             }
 
             if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
+                if ($item->name !== 'Aged Brie') {
                     if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
                         $item->quality = $item->quality - $item->quality;
-                    } else if ($item->quality > 0 && $item->name != 'Sulfuras, Hand of Ragnaros') {
+                    } else if ($item->quality > 0 && $item->name !== 'Sulfuras, Hand of Ragnaros') {
                         $item->quality = $item->quality - 1;
                     }
                 } else if ($item->quality < 50) {
