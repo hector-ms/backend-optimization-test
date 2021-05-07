902031ce0ad1e523ffb61e09ad04404a6764eb81
Héctor Muñoz <hector.9.1995@gmail.com>
2021-05-07 15:32:23 +0200

    Simplificación de operaciones y asignación de valores.

--- app/src/Service/GildedRoseUpdateQualityService.php
+++ app/src/Service/GildedRoseUpdateQualityService.php
@@ -25,31 +25,31 @@ class GildedRoseUpdateQualityService
         foreach ($this->items as $item) {
             if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
                 if ($item->quality > 0 && $item->name !== 'Sulfuras, Hand of Ragnaros') {
-                    $item->quality = $item->quality - 1;
+                    $item->quality--;
                 }
             } else if ($item->quality < 50) {
-                $item->quality = $item->quality + 1;
+                $item->quality++;
                 if ($item->name === 'Backstage passes to a TAFKAL80ETC concert' && $item->sell_in < 11) {
-                    $item->quality = $item->quality + 1;
+                    $item->quality++;
                     if ($item->sell_in < 6) {
-                        $item->quality = $item->quality + 1;
+                        $item->quality++;
                     }
                 }
             }
 
             if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
+                $item->sell_in--;
             }
 
             if ($item->sell_in < 0) {
                 if ($item->name !== 'Aged Brie') {
                     if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
-                        $item->quality = $item->quality - $item->quality;
+                        $item->quality = 0;
                     } else if ($item->quality > 0 && $item->name !== 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
+                        $item->quality--;
                     }
                 } else if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
+                    $item->quality++;
                 }
             }
         }
